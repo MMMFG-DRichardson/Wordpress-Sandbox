48f745e8aabd46b838ea77f39ca176e9411afdc2
wordpress-sandbox <write3me@gmail.com>
2016-08-08 09:27:30 -0500

    Initial commit

---
+++ themes/theme-for-mm-manufacturing/inc/jetpack.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Jetpack Compatibility File.
+ *
+ * @link https://jetpack.com/
+ *
+ * @package MMMFG
+ */
+
+/**
+ * Jetpack setup function.
+ *
+ * See: https://jetpack.com/support/infinite-scroll/
+ * See: https://jetpack.com/support/responsive-videos/
+ */
+function theme_for_mm_manufacturing_jetpack_setup() {
+	// Add theme support for Infinite Scroll.
+	add_theme_support( 'infinite-scroll', array(
+		'container' => 'main',
+		'render'    => 'theme_for_mm_manufacturing_infinite_scroll_render',
+		'footer'    => 'page',
+	) );
+
+	// Add theme support for Responsive Videos.
+	add_theme_support( 'jetpack-responsive-videos' );
+}
+add_action( 'after_setup_theme', 'theme_for_mm_manufacturing_jetpack_setup' );
+
+/**
+ * Custom render function for Infinite Scroll.
+ */
+function theme_for_mm_manufacturing_infinite_scroll_render() {
+	while ( have_posts() ) {
+		the_post();
+		if ( is_search() ) :
+		    get_template_part( 'template-parts/content', 'search' );
+		else :
+		    get_template_part( 'template-parts/content', get_post_format() );
+		endif;
+	}
+}
